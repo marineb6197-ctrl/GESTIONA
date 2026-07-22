9a5f99c4d5e0d4d21ae0f719c970befc5dd2af3b
marineb6197-ctrl <marine.b.6197@gmail.com>
2026-07-22 17:11:52 +0200

    GESTIONA V3.0

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='gestiona-v2-1';
+const CACHE='gestiona-v3-0';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/apple-touch-icon.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
